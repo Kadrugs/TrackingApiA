f081642e78a96e27fed5c8e62023369a9ef07cdf
Kadrugs <cgworkandadvice@gmail.com>
2022-05-30 11:07:21 -0300

    init commit

---
+++ TrackingApiA/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackingApiA.Models;
+
+namespace TrackingApiA.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext (DbContextOptions<IssueDbContext> options) :base(options)
+        {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
